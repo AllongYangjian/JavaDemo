a3c1ee5d9ae43720163a049c4425a483e05fece0
AllongYangjian <952801974@qq.com>
2020-10-09 17:23:50 +0800

    添加数据库配置

---
+++ SpringData/jpa/src/main/java/com/allong/springdata/config/DataSourceConfig.java
@@ -0,0 +1,21 @@
+package com.allong.springdata.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+/**
+ * Copyright (C), 2015-2020, 杭州奥朗信息科技有限公司
+ * FileName: DataSourceConfig
+ * Author:   YJ
+ * Date:     2020/10/9 15:34
+ * Description: 数据库配置
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * YJ       2020/10/9 15:34        1.0              描述
+ */
+public class DataSourceConfig {
+    //默认采用的是HikariDataSource
+
+}
--- SpringSecurity/app/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
+++ SpringSecurity/app/src/main/java/com/example/springsecurity/config/WebSecurityConfig.java
@@ -1,12 +1,20 @@
 package com.example.springsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.RememberMeServices;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenBasedRememberMeServices;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 /**
  * Copyright (C), 2015-2020, 杭州奥朗信息科技有限公司
@@ -22,6 +30,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Resource
+    private DataSource dataSource;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -34,7 +45,27 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .logout()
                 .permitAll();
+
+//        http.rememberMe()
+//                .rememberMeServices(rememberMeServices())
+//                .key("INTERNAL_SECRET_KEY");
     }
+
+//    @Bean
+//    public RememberMeServices rememberMeServices() {
+//        JdbcTokenRepositoryImpl rememberMeTokenRepository = new JdbcTokenRepositoryImpl();
+//        // 此处需要设置数据源，否则无法从数据库查询验证信息
+//        rememberMeTokenRepository.setDataSource(dataSource);
+//
+//        // 此处的 key 可以为任意非空值(null 或 "")，单必须和起前面
+//        // rememberMeServices(RememberMeServices rememberMeServices).key(key)的值相同
+//        PersistentTokenBasedRememberMeServices rememberMeServices =
+//                new PersistentTokenBasedRememberMeServices("INTERNAL_SECRET_KEY", userDetailsService, rememberMeTokenRepository);
+//
+//        // 该参数不是必须的，默认值为 "remember-me", 但如果设置必须和页面复选框的 name 一致
+//        rememberMeServices.setParameter("remember-me");
+//        return rememberMeServices;
+//    }
 
 
     @Autowired
