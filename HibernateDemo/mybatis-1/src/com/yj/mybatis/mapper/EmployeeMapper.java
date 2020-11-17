415e1f288a70c06bb111f4955065b9c00a8e93a3
AllongYangjian <952801974@qq.com>
2020-11-17 23:08:09 +0800

    Mybatis学习

---
+++ HibernateDemo/mybatis-1/src/com/yj/mybatis/mapper/EmployeeMapper.java
@@ -0,0 +1,18 @@
+package com.yj.mybatis.mapper;
+
+import com.yj.mybatis.domain.Employee;
+
+/**
+ * Copyright (C), 2015-2020, 杭州奥朗信息科技有限公司
+ * FileName: EmployeeMapper
+ * Author:   yangjian
+ * Date:     2020/11/17 21:38
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * yangjian       2020/11/17 21:38        1.0              描述
+ */
+public interface EmployeeMapper {
+
+    Employee findById(Integer id);
+}
