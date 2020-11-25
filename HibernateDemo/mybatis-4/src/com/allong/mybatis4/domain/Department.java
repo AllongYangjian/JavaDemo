559268e988b9be53726482780490ef8aa3304d19
AllongYangjian <952801974@qq.com>
2020-11-25 23:26:42 +0800

    mybatis动态sql，条件查询

---
+++ HibernateDemo/mybatis-4/src/com/allong/mybatis4/domain/Department.java
@@ -0,0 +1,66 @@
+package com.allong.mybatis4.domain;
+
+import java.util.List;
+
+/**
+ * Copyright (C), 2015-2020, 杭州奥朗信息科技有限公司
+ * FileName: Department
+ * Author:   yangjian
+ * Date:     2020/11/23 22:59
+ * Description: 科室
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * yangjian       2020/11/23 22:59        1.0              描述
+ */
+public class Department {
+
+    private Integer id;
+
+    private String name;
+
+    private String desc;
+
+    private List<Employee> emps;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<Employee> getEmps() {
+        return emps;
+    }
+
+    public void setEmps(List<Employee> emps) {
+        this.emps = emps;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", emps=" + emps +
+                '}';
+    }
+}
