620bbe4760161b0c5d0364160a06ba2e028762a3
AllongYangjian <952801974@qq.com>
2021-07-15 23:31:02 +0800

    初始化项目

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.Map;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        Map
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
